dcb86924077976d9be8e26703d685775e5246c17
HYEJUNJISOO <milan3676@naver.com>
2023-07-12 21:16:50 +0900

    0712 - 타임리프에 header,footer 공통파일로 묶음 / 스프링부트 배너 수정 /  마리아디비 테이블생성 및 커피메뉴 연동(오류발생)
    [DB 연동과정에서 ERROR 13624 발생]
    yml -> mapper location 관련오류 OR xml namespace 관련 오류인거같음 -> 수정중

--- Sailing/src/main/java/com/boot/sailing/v1/service/MenuSvc.java
+++ Sailing/src/main/java/com/boot/sailing/v1/service/MenuSvc.java
@@ -1,6 +1,8 @@
 package com.boot.sailing.v1.service;
 
+import com.boot.sailing.v1.dao.MenuDao;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,46 +14,60 @@ import java.util.Map;
 @Log4j2
 public class MenuSvc {
 
+    @Autowired
+    MenuDao menuDao;
+
     public MenuSvc() {
-        log.info("========== MenuSvc, 생성자 ============");
+        log.info("================ MenuSvc , 생성자 ===================");
     }
 
-    public List<Map<String,Object>> doList(){
-            // Data 만들기 - List,Map
-            List<Map<String,Object>> list=new ArrayList<>();
+    public List<Map<String, Object>> doList(){
 
-            Map<String, Object> map = new HashMap<>();
-            map.put("No", "1");
-            map.put("name", "아이스아메리카노");
-            map.put("kind", "커피");
-            map.put("price", "5,000");
-            map.put("reg_day", "2020.10.29");
-            map.put("mod_day", "2021.10.29");
-            list.add(map);
+        // Data 만들기 , List , Map
+        List<Map<String, Object>> list = menuDao.doList();
 
-            Map<String, Object> map2 = new HashMap<>();
+        log.info(list);
 
-            map2.put("No", "2");
-            map2.put("name", "카푸치노");
-            map2.put("kind", "커피");
-            map2.put("price", "6,000");
-            map2.put("reg_day", "2020.10.29");
-            map2.put("mod_day", "2021.10.29");
-            list.add(map2);
-
-            Map<String, Object> map3 = new HashMap<>();
-
-            map3.put("No", "3");
-            map3.put("name", "카페라떼");
-            map3.put("kind", "커피");
-            map3.put("price", "6,500");
-            map3.put("reg_day", "2020.10.29");
-            map3.put("mod_day", "2021.10.29");
-            list.add(map3);
-
-            log.info(list);
-
-            return list;
+        return list;
 
     }
+    public List<Map<String, Object>> doListOld(){
+
+        // Data 만들기 , List , Map
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("No", "1");
+        map.put("name", "아이스아메");
+        map.put("kind", "커피");
+        map.put("price", "5,000");
+        map.put("reg_day", "2020.10.29");
+        map.put("mod_day", "2021.10.29");
+        list.add(map);
+
+        Map<String, Object> map2 = new HashMap<>();
+
+        map2.put("No", "2");
+        map2.put("name", "카푸치노");
+        map2.put("kind", "커피");
+        map2.put("price", "6,000");
+        map2.put("reg_day", "2020.10.29");
+        map2.put("mod_day", "2021.10.29");
+        list.add(map2);
+
+        Map<String, Object> map3 = new HashMap<>();
+
+        map3.put("No", "3");
+        map3.put("name", "카페라떼");
+        map3.put("kind", "커피");
+        map3.put("price", "6,500");
+        map3.put("reg_day", "2020.10.29");
+        map3.put("mod_day", "2021.10.29");
+        list.add(map3);
+
+        log.info(list);
+
+        return list;
+    }
+
 }
